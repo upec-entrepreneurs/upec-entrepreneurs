214023bf9b37c3b8ee9c09bdad75dc5ba7dfa9f5
esuau <evan.suau@etu.u-pec.fr>
2018-12-01 20:02:09 +0100

    feat(navigation): Change component content when navigation occurs

--- src/app/components/events/events.component.spec.ts
+++ src/app/components/events/events.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MatToolbarModule } from '@angular/material';
 import { EventsComponent } from './events.component';
 
@@ -10,6 +12,8 @@ describe('EventsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule,
+        NoopAnimationsModule,
         MatToolbarModule
       ],
       declarations: [ EventsComponent ]
@@ -25,5 +29,11 @@ describe('EventsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the right title', () => {
+    expect(component['getTitle']('/events/upcoming')).toEqual('Évènements à venir');
+    expect(component['getTitle']('/events/past')).toEqual('Évènements passés');
+    expect(component['getTitle']('')).toEqual('Évènements ');
   });
 });
--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, Event } from '@angular/router';
+
 
 @Component({
   selector: 'app-events',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+
+  constructor( private _router: Router ) {
+    this._router.events.subscribe((event: Event) => {
+        if (event instanceof NavigationEnd) {
+          this.title = this.getTitle(this._router.url);
+        }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  private getTitle(url: string): string {
+    let title = 'Évènements ';
+    switch (url) {
+      case '/events/upcoming':
+        return title += 'à venir';
+      case '/events/past':
+        return title += 'passés';
+      default:
+        return title;
+    }
   }
 
 }
